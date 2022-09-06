771099a91ac4e521135c62f19e303254053228e7
Robert-96 <dezmereanrobert@gmail.com>
2022-09-07 00:22:05 +0300

    Update install-graphwalker.sh

--- install-graphwalker.sh
+++ install-graphwalker.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-echo "Install GraphWalker..."
-
 echo "Download GraphWalker CLI jar..."
-wget https://github.com/GraphWalker/graphwalker-project/releases/download/4.3.0/graphwalker-cli-4.3.0.jar > /dev/null
+wget https://github.com/GraphWalker/graphwalker-project/releases/download/4.3.0/graphwalker-cli-4.3.0.jar
 
+echo "Install GraphWalker CLI..."
 mkdir -p ~/.graphwalker
 mv graphwalker-cli-4.3.0.jar ~/.graphwalker/
 
 echo -e '#!/bin/bash\njava -jar ~/graphwalker/graphwalker-cli-4.3.0.jar "$@"' > ~/.graphwalker/graphwalker-cli.sh
-
 chmod +x ~/.graphwalker/graphwalker-cli.sh
 ln -s ~/.graphwalker/graphwalker-cli.sh /usr/local/bin/gw
