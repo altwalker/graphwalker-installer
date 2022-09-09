ac32575392ab899604ce72fbce50981bad2fce7a
Robert-96 <dezmereanrobert@gmail.com>
2022-09-09 21:54:18 +0300

    Update install-graphwalker.bat

--- install-graphwalker.bat
+++ install-graphwalker.bat
@@ -1,3 +1,5 @@
+setx PATH "%PATH%;C:\graphwalker" & :: Adds GraphWalker CLI to the current user PATH
+
 mkdir C:\graphwalker
 powershell -Command "[Net.ServicePointManager]::SecurityProtocol = 'tls12'; Invoke-WebRequest -Uri 'https://github.com/GraphWalker/graphwalker-project/releases/download/4.3.1/graphwalker-cli-4.3.1.jar' -outfile 'C:\graphwalker\graphwalker-cli-4.3.1.jar'" & :: Downloads GraphWalker CLI using powershell command Invoke-Request
 
@@ -5,5 +7,3 @@ powershell -Command "[Net.ServicePointManager]::SecurityProtocol = 'tls12'; Invo
 @echo @echo off> gw.bat
 @echo java -jar C:\graphwalker\graphwalker-cli-4.3.1.jar %*>> gw.bat
 @echo on
-
-setx PATH "%PATH%;C:\graphwalker" & :: Adds GraphWalker CLI to the current user PATH
